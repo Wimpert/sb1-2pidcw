b908d07ad190626606dd97b65e63e946c2fabc9d
Wimpert <holvoetwim@gmail.com>
2024-11-17 11:58:28 +0100

    removal function

--- src/main.ts
+++ src/main.ts
@@ -77,15 +77,49 @@ interface Registration {
           </label>
           @for (participant of registration.participants; track $index; let i =
           $index){
-          <div class="flexitems-center flex space-x-2">
+          <div class="flex items-center space-x-2">
             <input
               type="text"
               [(ngModel)]="registration.participants[i]"
               name="participant{{ i }}"
               placeholder="Deelnemer {{ i + 1 }}"
-              required
               class="mt-1 grow border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm p-2"
             />
+            <button
+              type="button"
+              (click)="removeParticipant(i)"
+              class="text-red-600 hover:text-red-900 underline"
+            >
+              <svg
+                fill="#000000"
+                version="1.1"
+                id="Capa_1"
+                xmlns="http://www.w3.org/2000/svg"
+                xmlns:xlink="http://www.w3.org/1999/xlink"
+                width="15px"
+                height="15px"
+                viewBox="0 0 408.483 408.483"
+                xml:space="preserve"
+                class="fill-gray-400"
+              >
+                <g>
+                  <g>
+                    <path
+                      d="M87.748,388.784c0.461,11.01,9.521,19.699,20.539,19.699h191.911c11.018,0,20.078-8.689,20.539-19.699l13.705-289.316
+			H74.043L87.748,388.784z M247.655,171.329c0-4.61,3.738-8.349,8.35-8.349h13.355c4.609,0,8.35,3.738,8.35,8.349v165.293
+			c0,4.611-3.738,8.349-8.35,8.349h-13.355c-4.61,0-8.35-3.736-8.35-8.349V171.329z M189.216,171.329
+			c0-4.61,3.738-8.349,8.349-8.349h13.355c4.609,0,8.349,3.738,8.349,8.349v165.293c0,4.611-3.737,8.349-8.349,8.349h-13.355
+			c-4.61,0-8.349-3.736-8.349-8.349V171.329L189.216,171.329z M130.775,171.329c0-4.61,3.738-8.349,8.349-8.349h13.356
+			c4.61,0,8.349,3.738,8.349,8.349v165.293c0,4.611-3.738,8.349-8.349,8.349h-13.356c-4.61,0-8.349-3.736-8.349-8.349V171.329z"
+                    />
+                    <path
+                      d="M343.567,21.043h-88.535V4.305c0-2.377-1.927-4.305-4.305-4.305h-92.971c-2.377,0-4.304,1.928-4.304,4.305v16.737H64.916
+			c-7.125,0-12.9,5.776-12.9,12.901V74.47h304.451V33.944C356.467,26.819,350.692,21.043,343.567,21.043z"
+                    />
+                  </g>
+                </g>
+              </svg>
+            </button>
           </div>
           }
           <button
@@ -101,7 +135,11 @@ interface Registration {
         <div class="options-container space-y-2">
           <h1 class="text-lg font-bold mb-4">Kies het type trot:</h1>
           <div
-            class="radio-option flex items-center bg-teal-300 rounded-lg p-3"
+            class="radio-option flex items-center rounded-lg p-2"
+            [ngClass]="{
+              'bg-slate-200': registration.option !== 'lang/lang',
+              'bg-teal-300': registration.option === 'lang/lang'
+            }"
           >
             <input
               type="radio"
@@ -109,6 +147,7 @@ interface Registration {
               name="option"
               value="lang/lang"
               [(ngModel)]="registration.option"
+              (change)="onOptionChange()"
               class="focus:ring-indigo-500 h-4 w-4 text-indigo-600"
             />
             <label for="langLang" class="ml-3 block text-gray-700"
@@ -117,8 +156,12 @@ interface Registration {
           </div>
 
           <div
-            class="transition ease-in-out delay-150 radio-option flex items-center bg-teal-300 rounded-lg p-3"
+            class="transition ease-in-out delay-150 radio-option flex items-center rounded-lg p-2"
             *ngFor="let opt of movingOptions; let i = index"
+            [ngClass]="{
+              'bg-slate-200': registration.option !== opt,
+              'bg-teal-300': registration.option === opt
+            }"
             (click)="moveOption($event, true)"
           >
             <input
@@ -128,7 +171,7 @@ interface Registration {
               [value]="opt"
               [(ngModel)]="registration.option"
               (mouseenter)="moveOption($event)"
-              (change)="moveOption($event, true)"
+              (change)="onOptionChange()"
               class="focus:ring-indigo-500 h-4 w-4 text-indigo-600"
             />
             <label
@@ -189,6 +232,12 @@ export class App {
     }
   }
 
+  removeParticipant(index: number) {
+    if (this.registration.participants.length > 1) {
+      this.registration.participants.splice(index, 1);
+    }
+  }
+
   moveOption(event: Event, checked = false) {
     const element = event.target as HTMLElement;
     const radioOptionDiv = element.closest(".radio-option") as HTMLElement;
@@ -216,6 +265,10 @@ export class App {
       this.registration.participants.every((name) => name.trim() !== "") &&
       this.registration.option === "lang/lang"
     );
+  }
+
+  onOptionChange() {
+    // This method is called when an option is selected
   }
 
   onSubmit() {
